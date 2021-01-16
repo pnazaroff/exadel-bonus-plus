c8b74a0491d8c3d79bb56ead80abd519c76806d5
EXAN <7742@mail.ru>
2021-01-16 23:43:22 +0300

    + add model Location

---
+++ ExadelBonusPlus.Services.Models/Location.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExadelBonusPlus.Services.Models
+{
+    public class Location
+    {
+        public string city { get; set; }
+
+        public string address { get; set; }
+
+        public string coordinates { get; set; }
+    }
+}
