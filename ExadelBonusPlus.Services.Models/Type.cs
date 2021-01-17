def237a224e7bf800a6d956249855bec86396f46
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-01-17 16:33:16 +0500

    Add model

---
+++ ExadelBonusPlus.Services.Models/Type.cs
@@ -0,0 +1,8 @@
+﻿namespace ExadelBonusPlus.Services.Models
+{
+    public class Type
+    {
+        public string Info { get; set; }
+        public float Percent { get; set; }
+    }
+}
---
+++ ExadelBonusPlus.Services.Models/Vendor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace ExadelBonusPlus.Services.Models
+{
+    public class Vendor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Info { get; set; }
+        public List<Location> Locations { get; set; }
+    }
+}
