1bbfa249a68a6aa05f3ecd8156646bdd86e3c1ae
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-01-18 11:46:07 +0500

    Add basic controller

---
+++ ExadelBonusPlus.WebApi/Controllers/VendorsController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using ExadelBonusPlus.Services.Models;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace ExadelBonusPlus.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VendorsController : ControllerBase
+    {
+        private IMongoDatabase database;
+        public VendorsController()
+        {
+            var client = new MongoClient("mongodb+srv://m001-student:m001-mongodb-basics@sandbox.yzrs7.mongodb.net/ExadelBonusPlus?retryWrites=true&w=majority");
+            //db name is hard coded
+            database = client.GetDatabase("ExadelBonusPlus");
+        }
+        [HttpGet("/api/Vendors")]
+        [SwaggerResponse((int)HttpStatusCode.OK)]
+        [SwaggerResponse((int)HttpStatusCode.NotFound)]
+        public async Task<IEnumerable<Vendor>> GetVendors()
+        {
+            var collection = database.GetCollection<Vendor>("Vendors");
+
+            return await collection.Find(new BsonDocument()).ToListAsync();
+            
+        }
+    }
+}
