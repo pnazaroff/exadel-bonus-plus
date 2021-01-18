29a515d025970f4e771fa5a4d004c1627de22237
AntonLip <liplianin.anton@yandex.ru>
2021-01-18 10:17:39 +0300

    Add User Comment History

---
+++ ExadelBonusPlus.Services.Models/HistoryOfUserManagment/HistoryOfUser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExadelBonusPlus.Services.Models.HistoryOfUserManagment
+{
+    /// <summary>
+    /// Represents a History of using discounts by the user.
+    /// </summary>
+    class HistoryOfUser
+    {
+        /// <summary>
+        /// Gets or sets identifier of history.
+        /// </summary>
+        public Guid id { get; set; }
+        /// <summary>
+        /// Gets or sets date of using discount.
+        /// </summary>
+        public DateTime dateOfAdd { get; set; }
+        /// <summary>
+        /// Gets or sets identifier of discount.
+        /// </summary>
+        public Guid idPromo { get; set; }
+        /// <summary>
+        /// Gets or sets identifier of user.
+        /// </summary>
+        public Guid idUser { get; set; }
+        /// <summary>
+        /// Gets or sets unique code.
+        /// </summary>
+        public string code { get; set; }
+    }
+}
---
+++ ExadelBonusPlus.Services.Models/UserManagment/UserApplication.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ExadelBonusPlus.Services.Models.UserManagment
+{
+    /// <summary>
+    /// Represents a User.
+    /// </summary>
+    class UserApplication : IdentityUser
+    {
+        /// <summary>
+        /// Gets or sets user's gender.
+        /// </summary>
+        public string gender { get; set; }
+        /// <summary>
+        /// Gets or sets user's lastname.
+        /// </summary>
+        public string family_name { get; set; }
+        /// <summary>
+        /// Gets or sets user's firstname.
+        /// </summary>
+        public string given_name { get; set; }
+        /// <summary>
+        /// Gets or sets user's middlename.
+        /// </summary>
+        public string middle_name { get; set; }
+        /// <summary>
+        /// Gets or sets in activ worker now.
+        /// </summary>
+        public bool isActiv { get; set; }
+        
+    }
+}
