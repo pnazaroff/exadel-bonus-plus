44ee0f0cca91fd99eb0e063c78fe8e59b5357b89
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-01-19 12:54:24 +0500

    Update connection configuration logic

--- ExadelBonusPlus.WebApi/Startup.cs
+++ ExadelBonusPlus.WebApi/Startup.cs
@@ -1,22 +1,37 @@
+using ExadelBonusPlus.Services;
+using ExadelBonusPlus.Services.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace ExadelBonusPlus.WebApi
 {
     public class Startup
     {
+        private IConfiguration _configuration;
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+                // requires using Microsoft.Extensions.Options
+                services.Configure<ExadelBonusDbSettings>(
+                _configuration.GetSection(nameof(ExadelBonusDbSettings)));
+
+            services.AddSingleton<IExadelBonusDbSettings>(sp =>
+                sp.GetRequiredService<IOptions<ExadelBonusDbSettings>>().Value);
+
+            services.AddSingleton<VendorService>();
+
+
             services.AddControllers();
 
             services.AddSwaggerGen(options =>
