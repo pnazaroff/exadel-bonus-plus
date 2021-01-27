5b88ba0d95bb1b50effbe9e6074555b9fe560737
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-01-27 16:20:05 +0500

    Add Vendor model

---
+++ ExadelBonusPlus.Services.Models/Vendor.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExadelBonusPlus.Services.Models
+{
+    public class Vendor
+    {
+        [BsonId]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Description { get; set; }
+        [BsonElement("Location")]
+        public List<Location> Locations { get; set; }
+    }
+}
