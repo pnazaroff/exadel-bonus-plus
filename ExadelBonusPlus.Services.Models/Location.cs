f6e860c53b51c3202810e7575099efe22ca3d2ef
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-01-27 16:13:17 +0500

    Add new model

---
+++ ExadelBonusPlus.Services.Models/Location.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExadelBonusPlus.Services.Models
+{
+    class Location
+    {
+        namespace ExadelBonusPlus.Services.Models
+    {
+        public class Location
+        {
+            public string City { get; set; }
+
+            public string Country { get; set; }
+
+            public string Address { get; set; }
+
+            public double Latitude { get; set; }
+
+            public double Longitude { get; set; }
+
+        }
+    }
+}
+}
