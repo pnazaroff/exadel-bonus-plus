12765f944775761021c76b21b549756b5ccb7511
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-01-28 10:52:42 +0500

    Add vendor repository contract

---
+++ ExadelBonusPlus.Services.Models/Interfaces/IVendorRepository.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ExadelBonusPlus.Services.Models.Interfaces
+{
+    public interface IVendorRepository : IRepository<Vendor, Guid>
+    {
+        //some specific functionality will be added soon...
+    }
+}
