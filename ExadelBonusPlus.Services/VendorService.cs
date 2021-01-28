d01c6931353fd555c7c70c8d61bbd0f9fa0d1e6d
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-01-28 10:53:08 +0500

    Add service implementation

---
+++ ExadelBonusPlus.Services/VendorService.cs
@@ -0,0 +1,36 @@
+﻿using ExadelBonusPlus.Services.Interfaces;
+using ExadelBonusPlus.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ExadelBonusPlus.Services
+{
+    public class VendorService : IVendorService
+    {
+        public Task<Vendor> AddVendorAsync(Vendor model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Vendor> DeleteVendorAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Vendor>> FindAllVendorsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Vendor> FindVendorByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Vendor> UpdateVendorAsync(Vendor model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
