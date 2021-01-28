8b73a1456bb27ec7d5b5c80fdb8111ee78468e23
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-01-28 10:51:44 +0500

    Add service

---
+++ ExadelBonusPlus.Services/Interfaces/IVendorService.cs
@@ -0,0 +1,20 @@
+﻿using ExadelBonusPlus.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ExadelBonusPlus.Services.Interfaces
+{
+    public interface IVendorService
+    {
+        Task<Vendor> AddVendorAsync(Vendor model);
+
+        Task<List<Vendor>> FindAllVendorsAsync();
+
+        Task<Vendor> FindVendorByIdAsync(Guid id);
+
+        Task<Vendor> UpdateVendorAsync(Vendor model);
+
+        Task<Vendor> DeleteVendorAsync(Guid id);
+    }
+}
