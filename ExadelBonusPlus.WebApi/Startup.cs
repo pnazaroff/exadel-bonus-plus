db5cbcff7a1895a6a2d1fcba42c53841e43bfbdf
Anton <liplianin.anton@yandex.ru>
2021-02-06 13:50:51 +0300

    Revert "Anton/authorization"

--- ExadelBonusPlus.WebApi/Startup.cs
+++ ExadelBonusPlus.WebApi/Startup.cs
@@ -1,12 +1,11 @@
-using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 using System.Threading.Tasks;
-using AutoMapper;
 using ExadelBonusPlus.Services.Models;
 
 namespace ExadelBonusPlus.WebApi
@@ -23,41 +22,16 @@ namespace ExadelBonusPlus.WebApi
         {
             services.Configure<MongoDbSettings>(_configuration.GetSection(
                 nameof(MongoDbSettings)));
-            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
             services.AddControllers();
-            
+
             services.AddSwaggerGen(c => {
                 c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
                 {
                     Version = "v1",
                     Title = "exadel-bonus-plus API V1",
                 });
-                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                {
-                    Description = "Input the JWT like: Bearer {your token}",
-                    Name = "Authorization",
-                    Scheme = "Bearer",
-                    BearerFormat = "JWT",
-                    In = ParameterLocation.Header,
-                    Type = SecuritySchemeType.ApiKey
-                });
-                c.AddSecurityRequirement(new OpenApiSecurityRequirement{
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            }
-                        },
-                        new string[] {}
-                    }
-                });
             });
-            
-            services.AddApiIdentityConfiguration(_configuration);
-
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -70,15 +44,16 @@ namespace ExadelBonusPlus.WebApi
                 };
                 app.UseDeveloperExceptionPage(developerExceptionPageOptions);
             }
-            
             app.UseStaticFiles();
-            app.UseRouting();
-            app.UseAuthorization();
-            app.UseAuthentication();
+
             app.UseSwagger();
+
             app.UseSwaggerUI(options =>
                 options.SwaggerEndpoint("/swagger/v1/swagger.json", "Exadel Bonus Plus API v1")
             );
+
+            app.UseRouting();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", context => {
