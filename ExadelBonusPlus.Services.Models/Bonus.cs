532c785f4236adb4f6b5dbfa358aa3a9b050a87d
EXAN <7742@mail.ru>
2021-02-08 12:30:42 +0300

    Add field Type in Bonus models

--- ExadelBonusPlus.Services.Models/Bonus.cs
+++ ExadelBonusPlus.Services.Models/Bonus.cs
@@ -6,35 +6,21 @@ namespace ExadelBonusPlus.Services.Models
     public class Bonus: IEntity<Guid>
     {
         public Guid Id { get; set; }
-
         public DateTime CreatedDate { get; set; }
-
         public Guid CreatorId { get; set; }
-
         public DateTime? ModifiedDate { get; set; }
-
         public Guid ModifierId { get; set; }
-        
         public string Title { get; set; }
-
         public string Description { get; set; }
-
+        public string Type { get; set; }
         public string Phone { get; set; }
-
         public Guid Company { get; set; }
-
         public DateTime DateStart { get; set; }
-
         public DateTime DateEnd { get; set; }
-
         public int Rating { get; set; }
-
         public List<Location> Locations { get; set; }
-
         public List<string> Tags { get; set; }
-
         public bool IsDeleted { get; set; }
-
         public bool IsActive { get; set; }
 
     }
--- ExadelBonusPlus.Services.Models/DTO/BonusDto.cs
+++ ExadelBonusPlus.Services.Models/DTO/BonusDto.cs
@@ -6,25 +6,16 @@ namespace ExadelBonusPlus.Services.Models
     public class BonusDto
     {
         public Guid Id { get; set; }
-
         public string Title { get; set; }
-
         public string Description { get; set; }
-
+        public string Type { get; set; }
         public string Phone { get; set; }
-
         public Guid Company { get; set; }
-
         public DateTime DateStart { get; set; }
-
         public DateTime DateEnd { get; set; }
-
         public int Rating { get; set; }
-
         public bool IsActive { get; set; }
-
         public List<Location> Locations { get; set; }
-
         public string[] Tags { get; set; }
     }
 }
