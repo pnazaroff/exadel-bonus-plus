6b8c2bacd81080abaf7b5cc5c7733e9307116193
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-02-10 18:41:05 +0500

    Add dto for adding object

---
+++ ExadelBonusPlus.Services.Models/DTO/AddVendorDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExadelBonusPlus.Services.Models.DTO
+{
+    public  class AddVendorDto
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Description { get; set; }
+    }
+}
