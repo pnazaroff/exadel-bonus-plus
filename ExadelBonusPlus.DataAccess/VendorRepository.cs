04f902da6f134a8c0930630d0072727866c42336
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-02-13 22:16:04 +0500

    Implement search by name (complete)

--- ExadelBonusPlus.DataAccess/VendorRepository.cs
+++ ExadelBonusPlus.DataAccess/VendorRepository.cs
@@ -1,11 +1,9 @@
 ﻿using ExadelBonusPlus.Services.Models;
 using Microsoft.Extensions.Options;
-using MongoDB.Bson;
 using MongoDB.Driver;
-using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading;
+using MongoDB.Driver.Linq;
 using System.Threading.Tasks;
 
 namespace ExadelBonusPlus.DataAccess
@@ -16,17 +14,9 @@ namespace ExadelBonusPlus.DataAccess
         {
 
         }
-        //should implement 
         public async Task<List<Vendor>> SearchVendorByNameAsync(string name, CancellationToken cancellationToken)
         {
-            var collection = GetCollection();
-            var indexKeysDefinition = Builders<Vendor>.IndexKeys.Ascending(vendor => vendor.Name);
-            var indexName = await collection.Indexes.CreateOneAsync(new CreateIndexModel<Vendor>(indexKeysDefinition));
-
-            var textFilter = Builders<Vendor>.Filter.Text(name);
-            var deletionFilter =
-                Builders<Vendor>.Filter.Eq(new ExpressionFieldDefinition<Vendor, bool>(x => x.IsDeleted), false);
-        
+            return await GetCollection().AsQueryable().Where(v=> v.Name.ToLower().Contains(name.ToLower())).ToListAsync(cancellationToken);
         }
 
     }
