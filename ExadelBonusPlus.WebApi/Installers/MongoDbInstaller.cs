2291831036c0ea0965ea65c50947dc37823b9d7d
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-02-20 14:53:29 +0500

    Fix wrong reference

--- ExadelBonusPlus.WebApi/Installers/MongoDbInstaller.cs
+++ ExadelBonusPlus.WebApi/Installers/MongoDbInstaller.cs
@@ -1,4 +1,4 @@
-﻿using AspNetCore.Identity.MongoDbCore.Infrastructure;
+﻿using ExadelBonusPlus.Services.Models;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
