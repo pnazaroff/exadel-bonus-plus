f528c65dc4b24a9b4cf86a4abe9e8b6c63959c03
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-02-20 14:54:02 +0500

    Refactor spaghetti code

--- ExadelBonusPlus.WebApi/Startup.cs
+++ ExadelBonusPlus.WebApi/Startup.cs
@@ -2,10 +2,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using System.Threading.Tasks;
-using ExadelBonusPlus.WebApi.Options;
 using ExadelBonusPlus.WebApi.Installers;
+using ExadelBonusPlus.WebApi.Configurators;
 
 namespace ExadelBonusPlus.WebApi
 {
@@ -24,49 +22,7 @@ namespace ExadelBonusPlus.WebApi
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                DeveloperExceptionPageOptions developerExceptionPageOptions = new DeveloperExceptionPageOptions
-                {
-                    SourceCodeLineCount = 5
-                };
-                app.UseDeveloperExceptionPage(developerExceptionPageOptions);
-            }
-            app.UseStaticFiles();
-
-            var swaggerOptions = new SwaggerOptions();
-            _configuration.GetSection(nameof(SwaggerOptions)).Bind(swaggerOptions);
-
-            app.UseSwagger(options =>
-            {
-                options.RouteTemplate = swaggerOptions.JsonRoute;
-            });
-
-            app.UseSwaggerUI(options =>
-            {
-                options.SwaggerEndpoint
-                (swaggerOptions.UiEndpoint, swaggerOptions.Description);
-            });
-
-            app.UseRouting();
-
-            
-            app.UseAuthentication();
-            app.UseAuthorization();
-            app.UseCors(builder => builder.AllowAnyOrigin()
-                                          .AllowAnyMethod()
-                                          .AllowAnyHeader());
-
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", context =>
-                {
-                    context.Response.Redirect("/swagger/");
-                    return Task.CompletedTask;
-                });
-                endpoints.MapControllers();
-            });
-        }
+            ConfiguratorExtensions.ConfigureServicesInAssembly(app, env, _configuration);
+         }
     }
 }
