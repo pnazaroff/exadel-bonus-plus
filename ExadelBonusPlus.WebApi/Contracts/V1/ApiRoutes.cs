530b29601735d7226cb18196c5652bb58d906927
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-02-20 12:50:37 +0500

    Create api routes class

---
+++ ExadelBonusPlus.WebApi/Contracts/V1/ApiRoutes.cs
@@ -0,0 +1,10 @@
+﻿namespace ExadelBonusPlus.WebApi.Contracts.V1
+{
+    public static class ApiRoutes
+    {
+        private const string _root = "api";
+        private const string _version = "v1";
+        private const string _base = _root + "/" + _version;
+        public const string Generic = _base + "/[controller]";
+    }
+}
